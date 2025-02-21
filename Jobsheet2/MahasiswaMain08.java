55bde1ac9a27813f24bcaf1ea8cd5d2d1625d943
ddickyy <dikiidarmawan05x2@gmail.com>
2025-02-21 10:01:02 +0700

    Update MahasiswaMain08.java

--- Jobsheet2/MahasiswaMain08.java
+++ Jobsheet2/MahasiswaMain08.java
@@ -10,6 +10,13 @@ public class MahasiswaMain08 {
         mhs1.ubahKelas("TI 1A");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa08 mhs2 = new Mahasiswa08 ("Dicky Darmawan","244107020037",3.86,"TI 1B");
+        mhs2.updateIpk(3.25);
+        mhs2.tampilkanInformasi();
+        
+        Mahasiswa08 mhsDicky = new Mahasiswa08("Samuel","2514702034",4.00,"TI 4A");
+        mhsDicky.tampilkanInformasi();
      }
 }
 

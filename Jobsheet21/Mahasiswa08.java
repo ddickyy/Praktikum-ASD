c7d9b2b264c1c2c5c6ee4933fc7c0d74eadeb128
ddickyy <dikiidarmawam05x2@gmail.com>
2025-02-21 09:02:47 +0700

    Selesai

--- Jobsheet21/Mahasiswa08.java
+++ Jobsheet21/Mahasiswa08.java
@@ -14,7 +14,11 @@ public class Mahasiswa08 {
     }
 
     void updateIpk (double ipkBaru) {
+        if (ipk >= 0.0 && ipk <= 4.0) { 
         ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
 
     String nilaiKinerja () {
---
+++ Jobsheet21/MahasiswaMain08.java
@@ -0,0 +1,13 @@
+package Jobsheet2;
+public class MahasiswaMain08 {
+    public static void main(String[] args) {
+        Mahasiswa08 mhs1 = new Mahasiswa08();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2441720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.tampilkanInformasi();
+    }
+}

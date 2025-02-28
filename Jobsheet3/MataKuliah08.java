010a5dbf614a42f821b93dd037d242f68658acee
ddickyy <dikiidarmawam05x2@gmail.com>
2025-02-28 16:33:35 +0700

    Update

--- Jobsheet3/MataKuliah08.java
+++ Jobsheet3/MataKuliah08.java
@@ -1,5 +1,3 @@
-
-
 public class MataKuliah08 {
     public String kode, nama;
     public int sks, jumlahJam;
@@ -9,5 +7,20 @@ public class MataKuliah08 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam= jumlahJam;
+    }
+
+    void tambahData (String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode        : " + kode);
+        System.out.println("Nama        : " + nama);
+        System.out.println("SKS         : " + sks);
+        System.out.println("Jumlah Jam  : " + jumlahJam);
+        System.out.println("------------------------------------");
     }
 }
--- Jobsheet3/MataKuliahDemo08.java
+++ Jobsheet3/MataKuliahDemo08.java
@@ -4,7 +4,11 @@ public class MataKuliahDemo08 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        MataKuliah08[] arrayOfMataKuliah = new MataKuliah08[3];
+        
+        System.out.print("Masukkan Jumlah Mata Kuliah: ");
+        int jmlmatkul = sc.nextInt();
+        
+        MataKuliah08[] arrayOfMataKuliah = new MataKuliah08[jmlmatkul];
         String kode, nama, dummy;
         int sks, jumlahJam;
 
@@ -23,15 +27,12 @@ public class MataKuliahDemo08 {
             System.out.println("---------------------------------------");
 
             arrayOfMataKuliah[i] = new MataKuliah08(kode, nama, sks, jumlahJam);
+            arrayOfMataKuliah[i].tambahData("ALJAB", "ALJABAR", 2, 4);
         }
 
         for (int i = 0; i < 3; i++) {
-            System.out.println("Data Mata Kuliah ke-" + (i+1));
-            System.out.println("Kode        : " + arrayOfMataKuliah[i].kode);
-            System.out.println("Nama        : " + arrayOfMataKuliah[i].nama);
-            System.out.println("SKS         : " + arrayOfMataKuliah[i].sks);
-            System.out.println("Jumlah Jam  : " + arrayOfMataKuliah[i].jumlahJam);
-            System.out.println("------------------------------------");
+            System.out.println("Data Mata Kuliah ke-" + (i + 1));
+            arrayOfMataKuliah[i].cetakInfo();
         }
     }
 }

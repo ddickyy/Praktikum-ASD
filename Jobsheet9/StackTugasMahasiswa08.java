e3ef9a8554edff81dbfd954c0a5ce2398a18b314
ddickyy <dikiidarmawam05x2@gmail.com>
2025-05-14 18:54:09 +0700

    Percobaan 2

--- Jobsheet9/StackTugasMahasiswa08.java
+++ Jobsheet9/StackTugasMahasiswa08.java
@@ -71,7 +71,21 @@ public class StackTugasMahasiswa08 {
     }
 
     public int hitungTugas() {
-        return top+1;
+        return top + 1;
+    }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi08 stack = new StackKonversi08();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
     }
 
 }
